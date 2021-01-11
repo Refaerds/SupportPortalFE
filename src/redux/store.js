c406f5f68df3688673e644bc8fb1ffd8ba998392
Refaerds <57571831+Refaerds@users.noreply.github.com>
2021-01-11 13:30:27 +0200

    modified persist config

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,6 +8,7 @@ import rootReducer from './root-reducer';
 const persistConfig = {
     key: 'root',
     storage,
+    blacklist: ['mytickets']
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
